75ce6eb02b1699889800440da28d14329e57f2a5
SAM V S <140595415+SAMVS2000@users.noreply.github.com>
2024-05-14 18:49:53 +0530

    Add files via upload

---
+++ subtwo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main() 
+{    
+
+    int a, b, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    // calculate the sum
+    sum = a - b;      
+    
+    printf("%d + %d = %d", a, b, sum);
+}
